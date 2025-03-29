4dcd567bbd0ad2c7d27177e3333bb7d3a7f7aa6c
sullis <github@seansullivan.com>
2025-03-28 20:20:54 -0700

    cp-kafka 7.8.0

--- src/test/java/io/github/sullis/kafka/playground/ContainerTest.java
+++ src/test/java/io/github/sullis/kafka/playground/ContainerTest.java
@@ -25,17 +25,21 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 @MethodSource("argProvider")
 record ContainerTest(String name, GenericContainer container) {
   private static final String APACHE_KAFKA_VERSION = "4.0.0";
-  private static final String CONFLUENT_VERSION = "7.9.0";
+  private static final String CONFLUENT_VERSION = "7.8.0";
   private static final KafkaContainer APACHE_KAFKA = new KafkaContainer(DockerImageName.parse("apache/kafka:" + APACHE_KAFKA_VERSION));
   private static final KafkaContainer APACHE_KAFKA_NATIVE_IMAGE = new KafkaContainer(DockerImageName.parse("apache/kafka-native:" + APACHE_KAFKA_VERSION));
   private static final ConfluentKafkaContainer CONFLUENT_PLATFORM_KAFKA = new ConfluentKafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:" + CONFLUENT_VERSION));
 
   private static Stream<Arguments> argProvider() {
     return Stream.of(
-        arguments("ApacheKafka", APACHE_KAFKA),
-        arguments("ApacheKafkaNativeImage", APACHE_KAFKA_NATIVE_IMAGE)
-        // arguments("ConfluentKafka", CONFLUENT_PLATFORM_KAFKA)
+        args(APACHE_KAFKA),
+        args(APACHE_KAFKA_NATIVE_IMAGE),
+        args(CONFLUENT_PLATFORM_KAFKA)
     );
+  }
+
+  private static Arguments args(GenericContainer genericCon) {
+    return arguments(genericCon.getDockerImageName(), genericCon);
   }
 
   @Test
