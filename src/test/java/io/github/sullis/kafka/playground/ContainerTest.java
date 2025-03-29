e9664d6035c5e0e4f06a9952d132f7bf8f37263d
sullis <github@seansullivan.com>
2025-03-28 19:30:49 -0700

    use JUnit ParameterizedClass

--- src/test/java/io/github/sullis/kafka/playground/ContainerTest.java
+++ src/test/java/io/github/sullis/kafka/playground/ContainerTest.java
@@ -1,12 +1,16 @@
 package io.github.sullis.kafka.playground;
 
-import org.junit.jupiter.api.Disabled;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedClass;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
 /*
@@ -16,7 +20,9 @@ import static org.assertj.core.api.Assertions.assertThat;
   https://java.testcontainers.org/modules/kafka/
 
  */
-public class ContainerTest {
+@ParameterizedClass
+@MethodSource("argProvider")
+record ContainerTest(String name, GenericContainer container) {
   private static final String APACHE_KAFKA_VERSION = "4.0.0";
   private static final String CONFLUENT_VERSION = "7.9.0";
   private static final KafkaContainer APACHE_KAFKA = new KafkaContainer(DockerImageName.parse("apache/kafka:" + APACHE_KAFKA_VERSION));
@@ -24,28 +30,15 @@ public class ContainerTest {
   private static final org.testcontainers.containers.KafkaContainer CONFLUENT_PLATFORM_KAFKA = new org.testcontainers.containers.KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:" + CONFLUENT_VERSION));
   private static final org.testcontainers.containers.KafkaContainer CONFLUENT_PLATFORM_KAFKA_WITH_KRAFT = new org.testcontainers.containers.KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:" + CONFLUENT_VERSION)).withKraft();
 
-  @Test
-  public void testConfluentPlatformKafka() {
-    validate(CONFLUENT_PLATFORM_KAFKA);
+  private static Stream<Arguments> argProvider() {
+    return Stream.of(
+        arguments("ApacheKafka", APACHE_KAFKA),
+        arguments("ApacheKafkaNativeImage", APACHE_KAFKA_NATIVE_IMAGE),
+        arguments("ConfluentKafka", CONFLUENT_PLATFORM_KAFKA));
   }
 
   @Test
-  @Disabled
-  public void testConfluentPlatformKafkaWithKraft() {
-    validate(CONFLUENT_PLATFORM_KAFKA_WITH_KRAFT);
-  }
-
-  @Test
-  public void testApacheKafka() {
-    validate(APACHE_KAFKA);
-  }
-
-  @Test
-  public void testApacheKafkaNativeImage() {
-    validate(APACHE_KAFKA_NATIVE_IMAGE);
-  }
-
-  private void validate(GenericContainer<?> container) {
+  public void validate() {
     container.start();
     assertThat(container.isRunning()).isTrue();
     container.stop();
