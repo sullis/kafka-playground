54918795a381b997639e792dafb9b32355d4f8c3
sullis <github@seansullivan.com>
2025-03-29 08:43:38 -0700

    add Kafka Consumer

--- src/test/java/io/github/sullis/kafka/playground/ContainerTest.java
+++ src/test/java/io/github/sullis/kafka/playground/ContainerTest.java
@@ -1,11 +1,13 @@
 package io.github.sullis.kafka.playground;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.stream.Stream;
-import org.apache.kafka.clients.KafkaClient;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -16,6 +18,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
 import org.testcontainers.utility.DockerImageName;
 
 import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
@@ -58,22 +61,47 @@ record ContainerTest(String name, GenericContainer container) {
     assertThat(container.isRunning()).isTrue();
     assertThat(container.getFirstMappedPort()).isGreaterThan(0);
 
-    String bootstrapServer = "127.0.0.1:" + container.getFirstMappedPort();
+    final String bootstrapServers = "127.0.0.1:" + container.getFirstMappedPort();
+    final Class serializerClass = org.apache.kafka.common.serialization.StringSerializer.class;
+    final Class deserializerClass = org.apache.kafka.common.serialization.StringDeserializer.class;
 
-    Properties config = new Properties();
-    config.put("client.id", UUID.randomUUID().toString());
-    config.put("bootstrap.servers", bootstrapServer);
-    config.put("acks", "all");
+    Properties producerConfig = new Properties();
+    producerConfig.put("client.id", UUID.randomUUID().toString());
+    producerConfig.put("bootstrap.servers", bootstrapServers);
+    producerConfig.put("acks", "all");
 
-    var serializerClassName = org.apache.kafka.common.serialization.StringSerializer.class.getName();
-    config.put(KEY_SERIALIZER_CLASS_CONFIG, serializerClassName);
-    config.put(VALUE_SERIALIZER_CLASS_CONFIG, serializerClassName);
+    producerConfig.put(KEY_SERIALIZER_CLASS_CONFIG, serializerClass);
+    producerConfig.put(VALUE_SERIALIZER_CLASS_CONFIG, serializerClass);
 
-    try (var producer = new KafkaProducer<String, String>(config)) {
-      var record = new ProducerRecord<>("topic123", null, "Hello", "World");
+    try (var producer = new KafkaProducer<String, String>(producerConfig)) {
+      var record = new ProducerRecord<>("topic123", "Hello", "World");
       var metadata = producer.send(record).get();
       assertThat(metadata.topic()).isEqualTo("topic123");
     }
+
+    Properties consumerConfig = new Properties();
+    consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "myGroupId");
+    consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, deserializerClass);
+    consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializerClass);
+
+    var consumer = new KafkaConsumer(consumerConfig);
+    consumer.subscribe(List.of("topic123"));
+
+    List<ConsumerRecord> allRecords = new ArrayList<>();
+
+    Awaitility.await()
+        .atMost(Duration.ofSeconds(5))
+        .untilAsserted(() -> {
+          var pollResult = consumer.poll(Duration.ofMillis(50));
+          if (!pollResult.isEmpty()) {
+            pollResult.iterator()
+                .forEachRemaining(r -> allRecords.add((ConsumerRecord) r));
+          }
+          assertThat(allRecords).hasSize(1);
+        });
+
+    consumer.close();
 
     container.stop();
     container.close();
